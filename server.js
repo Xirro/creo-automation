dc05c2babbcf37cb991f49cedbaed9502babd3c6
Xirro <huy.ngo.t05@gmail.com>
2023-12-06 17:21:01 -0600

    new file:   app/controllers/oldSearchSubmittal.js 	new file:   creoson/creoson.js 	deleted:    public/plugins/tinymce/themes/inlite/src/test/js/atomic/core/UrlTypeTest.js 	deleted:    scripts/createCounter.js 	modified:   scripts/createMBOMSchema.js 	deleted:    scripts/new.js 	modified:   scripts/newPnSchema.js 	deleted:    scripts/salesforceSchema.js 	modified:   server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ require('./app/routes/main.js')(app); //Main Router
 require('./app/routes/pdfDxfBinBom.js')(app); //PDF DXF BIN BOM Router
 require('./app/routes/submittal.js')(app); //Submittal Router
 require('./app/routes/mbom.js')(app); //MBOM router
-require('./app/routes/slimVAC.js')(app); //SlimVAC Router
+// require('./app/routes/slimVAC.js')(app); //SlimVAC Router
 require('./app/routes/partComparison.js')(app); //partComparison Router
 require('./app/routes/rename.js')(app); //Rename Router
 
