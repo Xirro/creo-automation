5d9151dc78652f0c1e6e60afdc70c68c9e36a03a
Xirro <hngo2@hawk.iit.edu>
2025-09-24 09:05:30 -0500

    chore: remove temporary parse_check script

--- scripts/parse_check.ps1
+++
@@ -1,5 +0,0 @@
-$path = 'c:\Users\huy.ngo\Documents\creo-automation\scripts\build-dist.ps1'
-$s = Get-Content -Raw -Path $path
-# Parse the script without executing it
-[System.Management.Automation.Language.Parser]::ParseInput($s, [ref]$null, [ref]$null) | Out-Null
-Write-Host 'PARSE_OK'
